7ac0fc9d11a506a5b0ece186a8ca16415f6795f6
chenxu89 <675269196@qq.com>
2015-05-28 08:57:29 +0800

    Initial Commit

---
+++ Reminders/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reminders
+//
+//  Created by 陈旭 on 5/28/15.
+//  Copyright (c) 2015 陈旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
