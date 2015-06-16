302b4dbf06a314cce07ae060d8753bfd3ea2eb69
chenxu89 <675269196@qq.com>
2015-06-16 17:05:33 +0800

    middle

--- Reminders/ImageViewController.m
+++ Reminders/ImageViewController.m
@@ -37,7 +37,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)close:(id)sender
+- (void)close:(UIGestureRecognizer *)gestureRecognizer
 {
     [self willMoveToParentViewController:nil];
     
